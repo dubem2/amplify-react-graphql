5d36cae32aed07e3a6c8301f520c707fc64327d6
George Owoh <owohgeorge@gmail.com>
2022-12-25 11:56:26 +0100

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import {
   Button,
   Flex,
@@ -69,14 +69,13 @@ const App = ({ signOut }) => {
   }
 
   return (
-    <View
-    <View
-       name="image"
-       as="input"
-       type="file"
-       style={{ alignSelf: "end" }}
-    />
-    <View className="App">
+    <View 
+      className="App"
+      name="image"
+      as="input"
+      type="file"
+      style={{ alignSelf: "end" }}  
+    >
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
@@ -114,6 +113,13 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
+                {note.image && (
+                  <Image
+                    src={note.image}
+                    alt={`visual aid for ${notes.name}`}
+                    style={{ width: 400 }}
+                  />
+        )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
@@ -123,29 +129,6 @@ const App = ({ signOut }) => {
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-  {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
 };
 
 export default withAuthenticator(App);
